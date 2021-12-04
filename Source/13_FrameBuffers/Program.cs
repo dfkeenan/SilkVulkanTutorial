26e4d6e6fabb8dfcebdcb58a45fbcd8a1c7a59d6
dfkeenan <dfkeenan@gmail.com>
2021-12-04 21:22:08 +1000

    Delete call to SilkMarshal.Free in places where it should not be.

--- Source/13_FrameBuffers/Program.cs
+++ Source/13_FrameBuffers/Program.cs
@@ -510,8 +510,6 @@ unsafe class HelloTriangleApplication
             PName = (byte*)SilkMarshal.StringToPtr("main")
         };
 
-        SilkMarshal.Free((nint)vertShaderStageInfo.PName);
-
         PipelineShaderStageCreateInfo fragShaderStageInfo = new()
         {
             SType = StructureType.PipelineShaderStageCreateInfo,
